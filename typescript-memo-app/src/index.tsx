9724587cbc05c3de92fbc8183c4e59fb6ca44e3a
nikawa2161 <nikawa.2161.t@gmail.com>
2022-09-04 21:20:32 +0900

    react.v18の記述に変更

--- typescript-memo-app/src/index.tsx
+++ typescript-memo-app/src/index.tsx
@@ -1,6 +1,8 @@
-import { render } from "react-dom";
+// import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { App } from "./components/App"
 import "./index.css";
 
-const rootElement = document.getElementById("root") as HTMLElement;
-render(<App/>, rootElement);
+const container = document.getElementById("root");
+const rootElement = createRoot(container!);
+rootElement.render(<App />);
