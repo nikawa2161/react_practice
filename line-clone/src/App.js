27e7b3023e68b829a6540fda5f58b4d4779973cc
nikawadori <nikawa.2161.t@gmail.com>
2022-10-09 12:24:33 +0900

    lineClone_FireBase

---
+++ line-clone/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import SignIn from './components/SignIn';
+import Line from './components/Line';
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from './firebase'
+
+function App() {
+  const [user] = useAuthState(auth);
+
+  return <div>{ user ? <Line /> : <SignIn />}</div>;
+}
+
+export default App;
