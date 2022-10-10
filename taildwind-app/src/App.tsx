da85e20bf6f859af74d33865f0d729948a9b21da
nikawadori <nikawa.2161.t@gmail.com>
2022-10-10 20:23:01 +0900

    page作成

--- taildwind-app/src/App.tsx
+++ taildwind-app/src/App.tsx
@@ -1,86 +1,31 @@
-import './index.css';
+import { Route, Routes } from "react-router-dom";
+import Home from "./components/Home";
+import Gaming from "./components/Gaming";
+import Learning from "./components/Learning";
+import Live from "./components/Live";
+import Movies from "./components/Movies";
+import Music from "./components/Music";
+import News from "./components/News";
+import Sports from "./components/Sports";
+import Trending from "./components/Trending";
+import "./index.css";
 
 function App() {
   return (
     <>
-      <div className="App">
-        <div className="relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12">
-          <div className="absolute inset-0 bg-center [mask-image:linear-gradient(180deg,white,rgba(255,255,255,0))]"></div>
-          <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-            <div className="mx-auto max-w-md">
-              <div className="divide-y divide-gray-300/50">
-                <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
-                  <p>
-                    An advanced online playground for Tailwind CSS, including
-                    support for things like:
-                  </p>
-                  <ul className="space-y-4">
-                    <li className="flex items-center">
-                      <svg
-                        className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <circle cx="12" cy="12" r="11" />
-                        <path
-                          d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
-                          fill="none"
-                        />
-                      </svg>
-                      <p className="ml-4">Customizing your file</p>
-                    </li>
-                    <li className="flex items-center">
-                      <svg
-                        className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <circle cx="12" cy="12" r="11" />
-                        <path
-                          d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
-                          fill="none"
-                        />
-                      </svg>
-                      <p className="ml-4">Extracting classes with</p>
-                    </li>
-                    <li className="flex items-center">
-                      <svg
-                        className="h-6 w-6 flex-none fill-sky-100 stroke-sky-500 stroke-2"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                      >
-                        <circle cx="12" cy="12" r="11" />
-                        <path
-                          d="m8 13 2.165 2.165a1 1 0 0 0 1.521-.126L16 9"
-                          fill="none"
-                        />
-                      </svg>
-                      <p className="ml-4">
-                        Code completion with instant preview
-                      </p>
-                    </li>
-                  </ul>
-                  <p>
-                    Perfect for learning how the framework works, prototyping a
-                    new idea, or creating a demo to share online.
-                  </p>
-                </div>
-                <div className="pt-8 text-base font-semibold leading-7">
-                  <p className="text-gray-900">
-                    Want to dig deeper into Tailwind?
-                  </p>
-                  <p>
-                    <a
-                      href="https://tailwindcss.com/docs"
-                      className="text-sky-500 hover:text-sky-600"
-                    >
-                      Read the docs &rarr;
-                    </a>
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
+      <div className="flex justify-center items-center w-screen h-screen">
+        <div className="w-11/12 h-5/6 md:w-9/12 md:h-5/6">
+          <Routes>
+            <Route path={`/`} element={<Home />} />
+            <Route path={`/gaming/`} element={<Gaming />} />
+            <Route path={`/learning/`} element={<Learning />} />
+            <Route path={`/live/`} element={<Live />} />
+            <Route path={`/movies/`} element={<Movies />} />
+            <Route path={`/music/`} element={<Music />} />
+            <Route path={`/news/`} element={<News />} />
+            <Route path={`/sports/`} element={<Sports />} />
+            <Route path={`/trending/`} element={<Trending />} />
+          </Routes>
         </div>
       </div>
     </>
