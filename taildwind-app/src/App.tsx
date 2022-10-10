d5e73baf73c27696cb1023adbb39c5a1cebb464a
nikawadori <nikawa.2161.t@gmail.com>
2022-10-10 21:38:03 +0900

    ページ遷移先の設定

--- taildwind-app/src/App.tsx
+++ taildwind-app/src/App.tsx
@@ -9,10 +9,12 @@ import News from "./components/News";
 import Sports from "./components/Sports";
 import Trending from "./components/Trending";
 import "./index.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
+      <Navbar />
       <div className="flex justify-center items-center w-screen h-screen">
         <div className="w-11/12 h-5/6 md:w-9/12 md:h-5/6">
           <Routes>
